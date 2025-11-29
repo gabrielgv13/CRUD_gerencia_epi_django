131801d39b90497e41eb0cb7f78f2a9f0f2b6068
geni <142359275+gabrielgv13@users.noreply.github.com>
2025-11-29 16:40:13 +0000

    models

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Colaborador(models.Model):
     nome = models.CharField(max_length=255)
     email = models.EmailField(unique=True, help_text="Email único do colaborador")
+    funcao = models.EmailField(max_length=255)
     # Adicione outros campos se precisar (ex: matrícula, cargo, etc.)
 
     def __str__(self):
